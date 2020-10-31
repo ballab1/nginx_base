17598b4856fa443beaefa0f4b3a883ac5ad56d76
Ballantyne, Bob <ballantyne.robert@gmail.com>
2020-10-31 09:01:48 -0400

    allow custom logging from environment variables

--- build/action_folders/07.run.startup/03.nginx_base.sh
+++ build/action_folders/07.run.startup/03.nginx_base.sh
@@ -14,6 +14,8 @@ chmod a+rx /var/lib/nginx/html
 chmod a+rx /var/lib/nginx
 chmod a+rx /var/lib
 
+declare  __file
+
 # nginx:  permit runtime config based on
 if [ "${NGINX_DEBUG:-0}" -ne 0 ] || [ -f /var/log/.nginx.debug ]; then
     if [ -d /usr/lib/nginx/modules.nodebug ]; then
@@ -25,7 +27,21 @@ if [ "${NGINX_DEBUG:-0}" -ne 0 ] || [ -f /var/log/.nginx.debug ]; then
     fi
 
     # add "error_log /var/log/nginx_error.log debug;"  to nginx.conf
-    declare  __file=/etc/nginx/nginx.conf
+    __file=/etc/nginx/nginx.conf
     term.log "    updating '${__file}' to log DEBUG information"'\n' 'white'
-    sed -Ei -e "s|^error_log\s+.*$|error_log /var/log/nginx_error.log debug;|" "$__file"
+    sed -Ei -e 's|^error_log\s+.*$|error_log /var/log/nginx_error.log debug;|' "$__file"
+fi
+
+if [ "${NGINX_LOG_ACCESS:-0}" != 0 ]; then
+    # add errors to syslog
+    __file=/etc/nginx/logging.settings
+    term.log "    updating '${__file}' to log to ${NGINX_LOG_ACCESS}"'\n' 'white'
+    sed -Ei -e 's|^access_log\s+.*$|access_log '"$NGINX_LOG_ACCESS"';|' "$__file"
+fi
+
+if [ "${NGINX_LOG_ERRORS:-0}" != 0 ]; then
+    # add access to syslog
+    __file=/etc/nginx/nginx.conf
+    term.log "    updating '${__file}' to log to ${NGINX_LOG_ERRORS}"'\n' 'white'
+    sed -Ei -e 's|^error_log\s+.*$|error_log '"$NGINX_LOG_ERRORS"';|' "$__file"
 fi
